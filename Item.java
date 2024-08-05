253ec9098bdd50493dfd4817744ff297509aaa66
YashDalal-7 <143619938+YashDalal-7@users.noreply.github.com>
2024-08-05 13:14:35 +0530

    Item.java

---
+++ Item.java
@@ -0,0 +1,28 @@
+public class Item {
+    private String name;
+    private int quantity;
+    private double price;
+
+    public Item(String name, int quantity, double price) {
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s (x%d) - $%.2f", name, quantity, price);
+    }
+}
